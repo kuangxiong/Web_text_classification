d373476388fb503951fb989291f350fc8ef4a242
kuangxiong <kuangxiong1993@163.com>
2020-12-28 14:20:00 +0800

    arrange file

--- web_text_classify/train.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-'''
----------------------------------------------------------
- File Name: train.py
- Author:kuangxiong
- Mail: kuangxiong@lsec.cc.ac.cn
- Created Time: Wed Dec  9 17:59:44 2020
----------------------------------------------------------
-'''
