3a93cdc2922aa2fe1d2ec4bb41593c63f4284e38
kuangxiong <kuangxiong1993@163.com>
2021-01-19 11:09:43 +0800

    代码规范化

--- web_text_classify/bert_data_preload.py
+++ web_text_classify/bert_data_preload.py
@@ -6,24 +6,25 @@
  Mail: kuangxiong@lsec.cc.ac.cn
  Created Time: Wed Dec  9 19:38:01 2020
 ---------------------------------------------------------
-''' 
+'''
+import csv
+import numpy as np
+import os
+import re
+import pickle
+import codecs
+
+from data_util import data_load
+from keras_bert import Tokenizer
+from sklearn.model_selection import train_test_split
+from config import GlobalConfig, BASE_PATH
 import tensorflow as tf
-from tensorflow import  keras
+from tensorflow import keras
 # bert模型的配置文件
 from BertBilstm import BertModelConfig
 
 import sys
 sys.path.append("..")
-from config import GlobalConfig, BASE_PATH
-from sklearn.model_selection import train_test_split
-from keras_bert import Tokenizer
-from data_preload import data_load
-import codecs
-import csv    
-import pickle
-import re
-import os
-import numpy as np
 
 
 def bert_get_text_id(BertModelConfig, train_text, training=True, max_length=BertModelConfig.max_len):
@@ -42,33 +43,35 @@ def bert_get_text_id(BertModelConfig, train_text, training=True, max_length=Bert
     with codecs.open(BertModelConfig.bert_vocab_path, 'r', 'utf8') as reader:
         for line in reader:
             token = line.strip()
-            token_dict[token]= len(token_dict)
-    
+            token_dict[token] = len(token_dict)
+
     tokenizer = Tokenizer(token_dict)
     data_X_ind, data_X_seg = [], []
     data_Y, data_id = [], []
     N = len(train_text)
     for i in range(N):
         tmp_text = train_text[i][1]
-        indices, segments = tokenizer.encode(first=tmp_text, max_len=BertModelConfig.max_len)
+        indices, segments = tokenizer.encode(
+            first=tmp_text, max_len=BertModelConfig.max_len)
         indices, segments = np.array(indices), np.array(segments)
         data_X_ind.append(indices)
         data_X_seg.append(segments)
-        if training==True:
+        if training == True:
             data_Y.append(train_text[i][2])
         else:
             data_id.append(train_text[i][0])
     data_X_ind = keras.preprocessing.sequence.pad_sequences(data_X_ind, maxlen=max_length, dtype='int32',
-    padding='post', truncating='post', value=0.)
-    
+                                                            padding='post', truncating='post', value=0.)
+
     data_X_seg = keras.preprocessing.sequence.pad_sequences(data_X_seg, maxlen=max_length, dtype='int32',
-    padding='post', truncating='post', value=0.)
-    if training==True:
-        return data_X_ind, data_X_seg, data_Y 
+                                                            padding='post', truncating='post', value=0.)
+    if training == True:
+        return data_X_ind, data_X_seg, data_Y
     else:
         return data_X_ind, data_X_seg, data_id
-    
-def load_bert_dataset(GlobalConfig, training=True, max_length = BertModelConfig.max_len):
+
+
+def bert_load_data(GlobalConfig, training=True, max_length=BertModelConfig.max_len):
     """
     生成训练数据集合
 
@@ -80,10 +83,10 @@ def load_bert_dataset(GlobalConfig, training=True, max_length = BertModelConfig.
         [type]: [训练集文件，测试集文件]
     """
     train_data, test_data = data_load(GlobalConfig)
-    
-    if training==True:
+
+    if training == True:
         train_text, train_seg, train_label_id = bert_get_text_id(BertModelConfig,
-            train_data, training,  max_length=max_length)
+                                                                 train_data, training,  max_length=max_length)
         train_id = list(map(int, train_label_id))
         train_label, N = [], len(train_label_id)
         for i in range(N):
@@ -93,7 +96,7 @@ def load_bert_dataset(GlobalConfig, training=True, max_length = BertModelConfig.
         return train_text, train_seg, train_label
     else:
         test_text, test_seg, test_id = bert_get_text_id(BertModelConfig,
-            test_data, training,  max_length=max_length)
+                                                        test_data, training,  max_length=max_length)
         return test_text, test_seg, test_id
 
 
@@ -107,16 +110,17 @@ def bert_get_X_and_Y_data(data_text, data_label):
     Returns:
         [list]: [训练集数据、训练集标签、验证集数据、验证集标签]
     """
-    train_X, dev_X, train_Y, dev_Y = train_test_split(data_text, data_label, \
-            test_size = 0.3, random_state = 5)
+    train_X, dev_X, train_Y, dev_Y = train_test_split(data_text, data_label,
+                                                      test_size=0.3, random_state=5)
     return train_X, train_Y, dev_X, dev_Y
 
-if __name__=='__main__':
-#    train_data, test_data = data_load(GlobalConfig)
-#    print(train_data[:5])
-#tmp1, tmp2 = bert_get_text_id(BertModelConfig, train_data)
-#    print(tmp1[5])
-#    print(tmp2[5])
+
+if __name__ == '__main__':
+    #    train_data, test_data = data_load(GlobalConfig)
+    #    print(train_data[:5])
+    #tmp1, tmp2 = bert_get_text_id(BertModelConfig, train_data)
+    #    print(tmp1[5])
+    #    print(tmp2[5])
     train_text, train_label = load_bert_dataset(GlobalConfig)
 #    print(len(train_text[5][0]))
 #    print(train_label[5])
---
+++ web_text_classify/data_util.py
@@ -0,0 +1,121 @@
+# -*- coding:utf-8 -*-
+'''
+---------------------------------------------------------
+ File Name: data_preload.py
+ Author:kuangxiong
+ Mail: kuangxiong@lsec.cc.ac.cn
+ Created Time: Wed Dec  9 19:38:01 2020
+---------------------------------------------------------
+'''
+import tensorflow as tf
+from tensorflow import  keras
+from models.Bilstm import ModelConfig
+from config import GlobalConfig, BASE_PATH
+from sklearn.model_selection import train_test_split
+import csv    
+import pickle
+import re
+import os
+
+
+def data_load(GlobalConfig):
+    """ 用于读取训练数据和测试数据
+
+    Args:
+        train_file ([str]): [训练数据]
+        train_file ([str]): [训练数据对应的标签数据]
+        test_file ([str]): [测试文件]
+
+    """
+
+    train_data, test_data=[], [] 
+    with open(GlobalConfig.train_path, 'rb') as f:
+#    with open("./data/nCoV_100k_train.labled.csv", 'rb') as f:
+        for line in f.readlines():
+            tmp = remove_redundant_inf(line)
+            split_data = next(csv.reader(tmp.splitlines(), skipinitialspace=True))
+#            split_data = tmp.split(',')
+            if split_data[6] in ['0','1','-1'] and split_data[3]!=[]:
+                train_data.append([split_data[0], split_data[3], split_data[6]])
+
+
+    with open(GlobalConfig.test_path, 'rb') as f:
+#    with open("./data/nCov_10k_test.csv", 'rb') as f:
+        for line in f.readlines():
+            tmp = remove_redundant_inf(line)
+            split_data = next(csv.reader(tmp.splitlines(), skipinitialspace=True))
+            test_data.append([split_data[0], split_data[3]])
+
+    return train_data[1:], test_data[1:]  
+
+    
+def check_contain_chinese(check_str):
+    """
+    字符串中是否包含中文字符
+
+    Args:
+        check_str ([str]): [一个字符串，判断是否包含中文，进而判断ID的合法性]
+    """
+    for e in check_str:
+        if u'\u4e00' <= e <= u'\u9fff':
+            return True 
+    return False
+
+
+def remove_html_punc(text):
+    """
+    去掉文本中的图片链接网页信息
+
+    Args:
+        text ([str]): [新闻网页文本]
+    """
+    # 去掉windown.open开头以 ;" 结尾的英文字段
+    tmp = re.sub(r"window.open\(.+;\"", "", text)
+    # 去掉 unload=开头，以；"结尾的字段
+    tmp = re.sub(r"onload=\".+;\"","", tmp)
+    # 去掉 src=开头，以"结尾的字段
+    tmp = re.sub(r"src=\".+\"","", tmp)
+    # 去掉style=开头, 以;"结尾的字段
+    tmp = re.sub(r"style=\".+;\"","", tmp)
+    # 去掉>图片开头，jpg结尾的字段
+    tmp = re.sub(r"/>图片:.+.jpg", "",tmp)
+    # 去掉换行符号”\n“
+    tmp = re.sub(r"\n", "", tmp)
+    # 去掉空格符号
+    tmp = re.sub(r" ","", tmp)
+    # 去掉"=750)"字样
+    tmp = re.sub(r"=750\)","", tmp)
+    return tmp
+
+def remove_redundant_inf(text):
+    """[去掉冗余信息]
+
+    Args:
+        text ([str]): [数据集合上的某一列]
+
+    Returns:
+        [str]: [去掉冗余信息后的文本数据]
+    """
+    tmp = text.decode('gbk', 'ignore')
+    tmp = re.sub("\r\n","",tmp)
+    tmp = re.sub("//@\w+?:", "", tmp)
+    tmp = re.sub("\?{3,}","?",tmp)
+    tmp = re.sub("(网页链接|展开全文)","",tmp)
+    tmp = re.sub("[a-z]{10,}","",tmp)
+    return tmp
+
+
+if __name__=='__main__':
+    train_data, test_data = data_load(GlobalConfig)
+    print(train_data[:5])
+    print(len(train_data))
+    word_dict = get_word_dict(train_data, test_data)
+    output = open('word_dict.pkl','wb')
+    pickle.dump(word_dict, output)
+    # train_text_id, _ ,test_text_id, _ = get_text_id(word_dict, train_data, test_data)
+    train_text, train_id = load_model_dataset(GlobalConfig, max_length = ModelConfig.max_len) 
+    # train_data, train_label, dev_data, dev_label = get_X_and_Y_data(train_text, train_id)
+    print(train_text[1], train_id[1])
+    # print(train_data)
+    # print(len(train_label), train_label)
+    # print(train_text_id[1])
